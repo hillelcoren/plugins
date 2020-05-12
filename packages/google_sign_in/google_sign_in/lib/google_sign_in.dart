5d797d5416770a4b11f5c873893c69eeced410ec
Hillel Coren <hillelcoren@gmail.com>
2020-05-12 18:27:22 +0300

    Enable offline OAuth support

--- packages/google_sign_in/google_sign_in/lib/google_sign_in.dart
+++ packages/google_sign_in/google_sign_in/lib/google_sign_in.dart
@@ -134,7 +134,7 @@ class GoogleSignInAccount implements GoogleIdentity {
   }
 
   @override
-  int get hashCode => hashValues(displayName, email, id, photoUrl, _idToken);
+  int get hashCode => hashValues(displayName, email, id, photoUrl, _idToken, _serverAuthCode);
 
   @override
   String toString() {
